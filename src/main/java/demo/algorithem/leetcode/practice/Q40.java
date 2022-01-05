789ab85eba3e2378fb4502645e2e03463cada20a
Lynx <xierz@foxmail.com>
2022-01-06 02:29:09 +0800

    q40

--- src/main/java/demo/algorithem/leetcode/practice/Q40.java
+++ src/main/java/demo/algorithem/leetcode/practice/Q40.java
@@ -6,28 +6,30 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * 回溯法
- *
  * @author xierz
- * @date 2022/1/5
+ * @date 2022/1/6
  */
-public class Q39 {
+public class Q40 {
+
     public static void main(String[] args) {
-        System.out.println(new Q39().combinationSum(new int[]{1}, 2));
+        System.out.println(new Q40().combinationSum2(new int[]{1, 2, 2, 5, 2}, 5));
     }
 
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(candidates);
         inner(result, candidates, target, 0, new LinkedList<>(), 0);
-        return new ArrayList<>(result);
+        return result;
     }
 
     private void inner(List<List<Integer>> result, int[] nums, int target, int current, LinkedList<Integer> linkedList, int start) {
         for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1]) {
+                continue;
+            }
             if (current + nums[i] < target) {
                 linkedList.addLast(nums[i]);
-                inner(result, nums, target, current + nums[i], linkedList, i);
+                inner(result, nums, target, current + nums[i], linkedList, i + 1);
                 linkedList.removeLast();
             } else if (current + nums[i] == target) {
                 linkedList.addLast(nums[i]);
