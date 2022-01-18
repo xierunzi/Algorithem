d0b3a9761e9832ee8b91eae0afc0229d4cb36a76
Lynx <xierz@foxmail.com>
2022-01-18 22:58:13 +0800

    q59

--- src/main/java/demo/algorithem/leetcode/practice/Q59.java
+++ src/main/java/demo/algorithem/leetcode/practice/Q59.java
@@ -1,60 +1,49 @@
 package demo.algorithem.leetcode.practice;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author xierz
- * @date 2022/1/17
+ * @date 2022/1/18
  */
-public class Q54 {
+public class Q59 {
     public static void main(String[] args) {
-        int[][] param = new int[][]{
-                new int[]{1, 2, 3},
-                new int[]{4, 5, 6},
-                new int[]{7, 8, 9}
-        };
-        System.out.println(new Q54().spiralOrder(param));
+        new Q59().generateMatrix(4);
     }
 
-    public List<Integer> spiralOrder(int[][] matrix) {
-        int[][] flags = new int[matrix.length][matrix[0].length];
+    public int[][] generateMatrix(int n) {
+        int[][] res = new int[n][n];
         // 0 right 1 down 2 left 3 up
         int symbol = 0;
-        List<Integer> result = new ArrayList<>();
         int i = 0;
         int j = 0;
-        int maxI = matrix.length;
-        int maxJ = matrix[0].length;
+        int num = 1;
         while (true) {
-            if (i < 0 || i >= maxI || j < 0 || j >= maxJ || flags[i][j] == 1) {
+            if (i < 0 || i >= n || j < 0 || j >= n || res[i][j] != 0) {
                 break;
             }
-            result.add(matrix[i][j]);
-            flags[i][j] = 1;
+            res[i][j] = num++;
             if (symbol == 0) {
-                if (j + 1 >= maxJ || flags[i][j + 1] == 1) {
+                if (j + 1 >= n || res[i][j + 1] != 0) {
                     i++;
                     symbol++;
                 } else {
                     j++;
                 }
             } else if (symbol == 1) {
-                if (i + 1 >= maxI || flags[i + 1][j] == 1) {
+                if (i + 1 >= n || res[i + 1][j] != 0) {
                     j--;
                     symbol++;
                 } else {
                     i++;
                 }
             } else if (symbol == 2) {
-                if (j - 1 < 0 || flags[i][j - 1] == 1) {
+                if (j - 1 < 0 || res[i][j - 1] != 0) {
                     i--;
                     symbol++;
                 } else {
                     j--;
                 }
             } else {
-                if (i - 1 < 0 || flags[i - 1][j] == 1) {
+                if (i - 1 < 0 || res[i - 1][j] != 0) {
                     j++;
                     symbol++;
                 } else {
@@ -65,7 +54,6 @@ public class Q54 {
                 symbol = 0;
             }
         }
-        return result;
+        return res;
     }
-
 }
